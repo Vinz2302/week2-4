0469f958d5c707f898ef7f592f5640d17c54aa08
Vinz <vincentkosasih2302@gmail.com>
2023-02-23 11:35:20 +0700

    change database

--- queries.js
+++ queries.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'postgres',
     host: '35.187.248.198',
-    database: 'Tral_Week1_Vincent',
+    database: 'trial_week2_4_vincent',
     password: 'd3v3l0p8015',
     port: 5432,
 })
